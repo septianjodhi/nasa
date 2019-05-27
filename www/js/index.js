bc014e829e3d20ebfb2e59b35ebd5fee737dfa34
septianjodhi <48472851+septianjodhi@users.noreply.github.com>
2019-05-27 10:03:59 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=xLDpGzmbBvBKUk75YkaoUUPoczcOCffK7MmQKfoE";
         
         $("#loadImage").click(function(){
             $.ajax({
